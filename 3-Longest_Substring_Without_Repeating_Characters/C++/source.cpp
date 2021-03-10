a566685a925651258c2342e58a594759f31425f2
gaiablade <cgandeg@gmail.com>
2021-03-10 18:46:14 -0500

    Problem 3: Longest Substr w/o rep. chars

---
+++ 3-Longest_Substring_Without_Repeating_Characters/C++/source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        /* e.g.
+        Input: s = "abcabcbb"
+        Output: 3
+        ......................
+        Input: s = "aab"
+        Output: 2
+        */
+        std::unordered_map<char, int> charMap;
+        int ret = 0;
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            if (i > s.length() - ret) return ret;
+            char& curr_character = s[j];
+            if (charMap.find(curr_character) != charMap.end()) {
+                i = std::max(i, charMap[curr_character] + 1);
+            }
+            ret = std::max(ret, j - i + 1);
+            charMap[curr_character] = j;
+        }
+        return ret;
+    }
+};
+
+int main() {
+    Solution s;
+    std::cout << s.lengthOfLongestSubstring("abcabcbb") << std::endl;
+}
